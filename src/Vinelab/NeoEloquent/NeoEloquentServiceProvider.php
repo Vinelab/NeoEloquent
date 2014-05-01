3765486730b37ca46afc50874c110206953a9a21
Abed Halawi <halawi.abed@gmail.com>
2014-05-01 22:30:38 +0300

    make the service provider set the connection resolver and event dispatcher to the model on boot

--- src/Vinelab/NeoEloquent/NeoEloquentServiceProvider.php
+++ src/Vinelab/NeoEloquent/NeoEloquentServiceProvider.php
@@ -13,6 +13,18 @@ class NeoEloquentServiceProvider extends ServiceProvider {
 	protected $defer = false;
 
 	/**
+	* Bootstrap the application events.
+	*
+	* @return void
+	*/
+	public function boot()
+	{
+		Model::setConnectionResolver($this->app['db']);
+
+		Model::setEventDispatcher($this->app['events']);
+	}
+
+	/**
 	 * Register the service provider.
 	 *
 	 * @return void
