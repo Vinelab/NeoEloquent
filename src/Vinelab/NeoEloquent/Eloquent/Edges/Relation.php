6410d63c26cba5b45dba0cf9c019db3a82a607ba
Abed Halawi <halawi.abed@gmail.com>
2014-05-21 18:46:39 +0300

    implement the abstract base edge Relation
    This class deals with models as parts of a relation, while being a
    light form of Eloquent Models that supports CRUDding attributes and
    saving itself to the database, in addition to utilities like
    transforming Eloquent Models to Nodes an exposing some of its data as
    Collections of Nodes or Models as requested.

---
+++ src/Vinelab/NeoEloquent/Eloquent/Edges/Relation.php
@@ -0,0 +1,416 @@
+<?php namespace Vinelab\NeoEloquent\Eloquent\Edges;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Collection;
+use Vinelab\NeoEloquent\Eloquent\Model;
+use Vinelab\NeoEloquent\Eloquent\Builder;
+
+abstract class Relation {
+
+    /**
+     * The database connection.
+     *
+     * @var \Vinelab\NeoEloquent\Connection
+     */
+    protected $connection;
+
+    /**
+     * The database client.
+     *
+     * @var \Everyman\Neo4j\Client
+     */
+    protected $client;
+
+    /**
+     * The left side Model of the relationship.
+     *
+     * @var \Vinelab\NeoEloquent\Eloquent\Model
+     */
+    protected $parent;
+
+    /**
+     * The right side Model of the relationship.
+     *
+     * @var \Vinelab\NeoEloquent\Eloquent\Model
+     */
+    protected $related;
+
+    /**
+     * The start Node of the relationship,
+     * represented by the $parent Model.
+     *
+     * @var \Everyman\Neo4j\Node
+     */
+    protected $start;
+
+    /**
+     * The end Node of the relationship,
+     * represented by the $related Model.
+     *
+     * @var \Everyman\Neo4j\Node
+     */
+    protected $end;
+
+    /**
+     * The relationship type.
+     *
+     * @var string
+     */
+    protected $type;
+
+    /**
+     * Relations can also have attributes.
+     *
+     * @var array
+     */
+    protected $attributes = array();
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = array();
+
+    /**
+     * Holds the decision on whether
+     * this relation is unique or
+     * there can be many of it.
+     *
+     * @var boolean
+     */
+    protected $unique = false;
+
+    /**
+     * The relationship instance.
+     *
+     * @var \Everyman\Neo4j\
+     */
+    protected $relation;
+
+    /**
+     * The name of the "created at" column.
+     *
+     * @var string
+     */
+    const CREATED_AT = 'created_at';
+
+    /**
+     * The name of the "updated at" column.
+     *
+     * @var string
+     */
+    const UPDATED_AT = 'updated_at';
+
+    /**
+     * Create a new Relation instance.
+     *
+     * @param Builder $query
+     * @param Model   $parent
+     * @param Model   $related
+     * @param string  $type
+     */
+    public function __construct(Builder $query, Model $parent, Model $related, $type, $attributes = array(), $unique = false)
+    {
+        $this->type       = $type;
+        $this->parent     = $parent;
+        $this->related    = $related;
+        $this->unique     = $unique;
+        $this->attributes = $attributes;
+
+        $this->connection = $parent->getConnection();
+        $this->client = $this->connection->getClient();
+
+        $this->initRelation();
+    }
+
+    /**
+     * Initialize the relationship setting the start node,
+     * end node and relation type.
+     *
+     * @return void
+     */
+    abstract public function initRelation();
+
+    /**
+     * Save the relationship to the database.
+     *
+     * @return boolean
+     */
+    public function save()
+    {
+        // Go through the properties and assign them
+        // to the relation.
+        foreach ($this->toArray() as $key => $value)
+        {
+            $this->relation->setProperty($key, $value);
+        }
+
+        $saved = $this->relation->save();
+
+        return  $saved ? true : false;
+    }
+
+    /**
+     * Remove the relationship from the database.
+     *
+     * @return  boolean
+     */
+    public function delete()
+    {
+        if ( ! is_null($this->relation))
+        {
+            $deleted = $this->relation->delete();
+
+            return $deleted ? true : false;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the Neo4j relationship object.
+     *
+     * @return \Everyman\Neo4j\Relationship
+     */
+    public function getReal()
+    {
+        return $this->relation;
+    }
+
+    /**
+     * Set a given attribute on the relation.
+     *
+     * @param  string  $key
+     * @param  mixed   $value
+     * @return void
+     */
+    public function setAttribute($key, $value)
+    {
+        $this->attributes[$key] = $value;
+    }
+
+    /**
+     * Get an attribute from the relation.
+     *
+     * @param  string  $key
+     * @return mixed
+     */
+    public function getAttribute($key)
+    {
+        if (array_key_exists($key, $this->attributes))
+        {
+            $value = $this->attributes[$key];
+
+            if (in_array($key, $this->getDates()))
+            {
+                return $this->asDateTime($value);
+            }
+
+            return $value;
+        }
+    }
+
+    /**
+     * Get the attributes that should be converted to dates.
+     *
+     * @return array
+     */
+    public function getDates()
+    {
+        $defaults = array(static::CREATED_AT, static::UPDATED_AT);
+
+        return array_merge($this->dates, $defaults);
+    }
+
+    /**
+     * Set all the attributes of this relation.
+     *
+     * @param array $attributes
+     */
+    public function setRawAttributes(array $attributes)
+    {
+        $this->attributes = $attributes;
+    }
+
+    /**
+     * Get all the attributes of this relation
+     *
+     * @return mixed
+     */
+    public function getAttributes()
+    {
+        return $this->attributes;
+    }
+
+    /**
+     * Get the Models of this relation.
+     *
+     * @return \Illuminate\Database\Collection
+     */
+    public function getModels()
+    {
+        return new Collection(array($this->parent, $this->related));
+    }
+
+    /**
+     * Get the Nodes of this relation.
+     *
+     * @return \Illuminate\Database\Collection
+     */
+    public function getNodes()
+    {
+        return new Collection(array($this->start, $this->end));
+    }
+
+    /**
+     * Determine whether this relationship is unique.
+     *
+     * @return boolean
+     */
+    public function isUnique()
+    {
+        return $this->unique;
+    }
+
+    /**
+     * Return a timestamp as DateTime object.
+     *
+     * @param  mixed  $value
+     * @return \Carbon\Carbon
+     */
+    protected function asDateTime($value)
+    {
+        // If this value is an integer, we will assume it is a UNIX timestamp's value
+        // and format a Carbon object from this timestamp. This allows flexibility
+        // when defining your date fields as they might be UNIX timestamps here.
+        if (is_numeric($value))
+        {
+            return Carbon::createFromTimestamp($value);
+        }
+
+        // If the value is in simply year, month, day format, we will instantiate the
+        // Carbon instances from that format. Again, this provides for simple date
+        // fields on the database, while still supporting Carbonized conversion.
+        elseif (preg_match('/^(\d{4})-(\d{2})-(\d{2})$/', $value))
+        {
+            return Carbon::createFromFormat('Y-m-d', $value)->startOfDay();
+        }
+
+        // Finally, we will just assume this date is in the format used by default on
+        // the database connection and use that format to create the Carbon object
+        // that is returned back out to the developers after we convert it here.
+        elseif ( ! $value instanceof DateTime)
+        {
+            $format = $this->getDateFormat();
+
+            return Carbon::createFromFormat($format, $value);
+        }
+
+        return Carbon::instance($value);
+    }
+
+    /**
+     * Convert the model instance to an array.
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return (array) $this->attributes;
+    }
+
+    /**
+     * Get the left node of the relationship.
+     *
+     * @return \Everyman\Neo4j\Node
+     */
+    public function getStartNode()
+    {
+        return $this->start;
+    }
+
+    /**
+     * Get the end Node of the relationship.
+     *
+     * @return \Everyman\Neo4j\Node
+     */
+    public function getEndNode()
+    {
+        return $this->end;
+    }
+
+    /**
+     * Convert a model to a Node object.
+     *
+     * @param  \Vinelab\NeoEloquent\Eloquent\Model $model
+     * @return \Everyman\Neo4j\Node
+     */
+    public function asNode(Model $model)
+    {
+        $node = $this->client->makeNode();
+
+        // If the key name of the model is 'id' we will need to set it properly with setId()
+        // since setting it as a regular property with setProperty() won't cut it.
+        if ($model->getKeyName() == 'id')
+        {
+            $node->setId($model->getKey());
+        }
+
+        // In this case the dev has chosen a different primary key
+        // so we use it insetead.
+        else
+        {
+            $node->setProperty($model->getKeyName(), $model->getKey());
+        }
+
+        return $node;
+    }
+
+    /**
+     * Get the current connection name for the model.
+     *
+     * @return string
+     */
+    public function getConnectionName()
+    {
+        return $this->connection;
+    }
+
+    /**
+     * Set the connection associated with the model.
+     *
+     * @param  string  $name
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function setConnection($name)
+    {
+        $this->connection = $name;
+
+        return $this;
+    }
+
+    /**
+     * Dynamically set attributes on the relation.
+     *
+     * @param  string  $key
+     * @param  mixed   $value
+     * @return void
+     */
+    public function __set($key, $value)
+    {
+        $this->setAttribute($key, $value);
+    }
+
+    /**
+     * Dynamically retrieve attributes on the relation.
+     *
+     * @param  string  $key
+     * @return mixed
+     */
+    public function __get($key)
+    {
+        return $this->getAttribute($key);
+    }
+
+}
