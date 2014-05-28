03801505a7fc4afcd4411742b48e76b057c0255e
Abed Halawi <halawi.abed@gmail.com>
2014-05-28 16:58:23 +0300

    introduce the abstract Delegate class
    The Delegate will be the base of every class that needs to communicate
    with Neo4j’s client including transforming models into Nodes and
    keeping hold of the database connection and other most common Neo4j
    functionalities.

---
+++ src/Vinelab/NeoEloquent/Eloquent/Edges/Delegate.php
@@ -0,0 +1,193 @@
+<?php namespace Vinelab\NeoEloquent\Eloquent\Edges;
+
+use Vinelab\NeoEloquent\Connection;
+use Vinelab\NeoEloquent\Eloquent\Model;
+use Vinelab\NeoEloquent\QueryException;
+use Vinelab\NeoEloquent\Eloquent\Builder;
+
+abstract class Delegate {
+
+     /**
+     * The Eloquent builder instance.
+     *
+     * @var \Vinelab\NeoEloquent\Eloquent\Builder
+     */
+    protected $query;
+
+    /**
+     * The database connection.
+     *
+     * @var \Vinelab\NeoEloquent\Connection
+     */
+    protected $connection;
+
+    /**
+     * The database client.
+     *
+     * @var \Everyman\Neo4j\Client
+     */
+    protected $client;
+
+    /**
+     * Create a new delegate instance.
+     *
+     * @param \Vinelab\NeoEloquent\Eloquent\Builder $query
+     * @param \Vinelab\NeoEloquent\Eloquent\Model   $parent
+     */
+    public function __construct(Builder $query)
+    {
+        $this->query  = $query;
+        $model = $query->getModel();
+
+        // Setup the database connection and client.
+        $this->connection = $model->getConnection();
+        $this->client = $this->connection->getClient();
+    }
+
+    /**
+     * Get a new Finder instance.
+     *
+     * @return \Vinelab\NeoEloquent\Eloquent\Edges\Finder
+     */
+    public function newFinder()
+    {
+        return new Finder($this->query);
+    }
+
+    /**
+     * Make a new Relationship instance.
+     *
+     * @param  string $type
+     * @param  \Vinelab\NeoEloquent\Eloquent\Model $startModel
+     * @param  \Vinelab\NeoEloquent\Eloquent\Model $endModel
+     * @param  array  $properties
+     * @return \Everyman\Neo4j\Relationship
+     */
+    protected function makeRelationship($type, $startModel, $endModel, $properties = array())
+    {
+        return $this->client
+            ->makeRelationship()
+            ->setType($this->type)
+            ->setStartNode($this->start)
+            ->setEndNode($this->end)
+            ->setProperties($this->attributes);
+    }
+
+    /**
+     * Start a batch operation with the database.
+     *
+     * @return \Everyman\Neo4j\Batch
+     */
+    public function prepareBatch()
+    {
+        return $this->client->startBatch();
+    }
+
+    /**
+     * Commit the started batch operation.
+     *
+     * @return boolean
+     *
+     * @throws  \Vinelab\NeoEloquent\QueryException If no open batch to commit.
+     */
+    public function commitBatch()
+    {
+        try {
+
+            return $this->client->commitBatch();
+
+        } catch (\Exception $e)
+        {
+            throw new QueryException('Error committing batch operation.', array(), $e);
+        }
+    }
+
+    /**
+     * Get the direction value from the Neo4j
+     * client according to the direction set on
+     * the inheriting class,
+     *
+     * @param  string $direction
+     * @return string
+     *
+     * @throws UnknownDirectionException If the specified $direction is not one of in, out or inout
+     */
+    public function getRealDirection($direction)
+    {
+        if ($direction == 'in' or $direction == 'out')
+        {
+            $direction = ucfirst($direction);
+
+        } elseif ($direction == 'any')
+        {
+            $direction = 'All';
+
+        } else
+        {
+            throw new UnknownDirectionException($direction);
+        }
+
+        $direction = "Direction". $direction;
+
+        return constant("Everyman\Neo4j\Relationship::". $direction);
+    }
+
+    /**
+     * Convert a model to a Node object.
+     *
+     * @param  \Vinelab\NeoEloquent\Eloquent\Model $model
+     * @return \Everyman\Neo4j\Node
+     */
+    public function asNode(Model $model)
+    {
+        $node = $this->client->makeNode();
+
+        // If the key name of the model is 'id' we will need to set it properly with setId()
+        // since setting it as a regular property with setProperty() won't cut it.
+        if ($model->getKeyName() == 'id')
+        {
+            $node->setId($model->getKey());
+        }
+
+        // In this case the dev has chosen a different primary key
+        // so we use it insetead.
+        else
+        {
+            $node->setProperty($model->getKeyName(), $model->getKey());
+        }
+
+        return $node;
+    }
+
+    /**
+     * Get the NeoEloquent connection for this relation.
+     *
+     * @return \Vinelab\NeoEloquent\Connection
+     */
+    public function getConnection()
+    {
+        return $this->connection;
+    }
+
+    /**
+     * Set the database connection.
+     *
+     * @param  \Vinelab\NeoEloquent\Connection  $name
+     * @return void
+     */
+    public function setConnection(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    /**
+     * Get the current connection name.
+     *
+     * @return string
+     */
+    public function getConnectionName()
+    {
+        return $this->query->getModel()->getConnectionName();
+    }
+
+}
