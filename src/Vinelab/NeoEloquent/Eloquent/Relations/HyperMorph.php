0cc61e1827f567fb8f968a0d95eb1a4bad0202ff
Abed Halawi <abed.halawi@vinelab.com>
2014-06-06 17:48:58 +0300

    modify the direction of the HyperMorph relationship after the change in BelongsToMany.
    BelongsToMany used to have outgoing relationships until discovered that it shouldn't
    be the case, and since HyperMorph is extending that relationship it should now reverse
    the direction and constraints to match in OUTGOING instead.

--- src/Vinelab/NeoEloquent/Eloquent/Relations/HyperMorph.php
+++ src/Vinelab/NeoEloquent/Eloquent/Relations/HyperMorph.php
@@ -25,6 +25,13 @@ class HyperMorph extends BelongsToMany {
     protected $morphType;
 
     /**
+     * The edge direction of this relatioship.
+     *
+     * @var string
+     */
+    protected $edgeDirection = 'out';
+
+    /**
      * Create a new HyperMorph relationship.
      *
      * @param \Vinelab\NeoEloquent\Eloquent\Builder $query
@@ -41,6 +48,85 @@ class HyperMorph extends BelongsToMany {
         $this->morphType = $morphType;
 
         parent::__construct($query, $parent, $type, $key, $relation);
+    }
+
+        /**
+     * Set the base constraints on the relation query.
+     *
+     * @return void
+     */
+    public function addConstraints()
+    {
+        if (static::$constraints)
+        {
+            /**
+             * For has one relationships we need to actually query on the primary key
+             * of the parent model matching on the OUTGOING relationship by name.
+             *
+             * We are trying to achieve a Cypher that goes something like:
+             *
+             * MATCH (user:`User`), (user)-[:PHONE]->(phone:`Phone`)
+             * WHERE id(user) = 86234
+             * RETURN phone;
+             *
+             * (user:`User`) represents a matching statement where
+             * 'user' is the parent Node's placeholder and '`User`' is the parentLabel.
+             * All node placeholders must be lowercased letters and will be used
+             * throught the query to represent the actual Node.
+             *
+             * Resulting from:
+             * class User extends NeoEloquent {
+             *
+             *     public function phone()
+             *     {
+             *          return $this->hasOne('Phone', 'PHONE');
+             *     }
+             * }
+            */
+
+            // Get the parent node's placeholder.
+            $parentNode = $this->getParentNode();
+            // Tell the query that we only need the related model returned.
+            $this->query->select($this->relation);
+            // Set the parent node's placeholder as the RETURN key.
+            $this->query->getQuery()->from = array($parentNode);
+            // Build the MATCH ()-[]->() Cypher clause.
+            $this->query->matchOut($this->parent, $this->related, $this->relation, $this->foreignKey, $this->localKey, $this->parent->{$this->localKey});
+            // Add WHERE clause over the parent node's matching key = value.
+            $this->query->where($this->localKey, '=', $this->parent->{$this->localKey});
+        }
+    }
+
+    /**
+     * Set the constraints for an eager load of the relation.
+     *
+     * @param  array  $models
+     * @return void
+     */
+    public function addEagerConstraints(array $models)
+    {
+        /**
+         * We'll grab the primary key name of the related models since it could be set to
+         * a non-standard name and not "id". We will then construct the constraint for
+         * our eagerly loading query so it returns the proper models from execution.
+         */
+
+        // Grab the parent node placeholder
+        $parentNode = $this->query->getQuery()->modelAsNode($this->parent->getTable());
+
+        // Tell the builder to select both models of the relationship
+        $this->query->select($this->relation, $parentNode);
+
+        // Setup for their mutation so they don't breed weird stuff like... humans ?!
+        $this->query->addManyMutation($this->relation, $this->related);
+        $this->query->addManyMutation($parentNode, $this->parent);
+
+        // Set the parent node's placeholder as the RETURN key.
+        $this->query->getQuery()->from = array($parentNode);
+        // Build the MATCH ()-[]->() Cypher clause.
+        $this->query->matchOut($this->parent, $this->related, $this->relation, $this->foreignKey, $this->localKey, $this->parent->{$this->localKey});
+        // Add WHERE clause over the parent node's matching keys [values...].
+        $this->query->whereIn($this->localKey, $this->getKeys($models));
     }
 
     public function edge(Model $model = null)
