cd496ab1fd5bef85a8c44317d3068c3bda826d97
David Soto <dsoto@tpipr.com>
2016-03-14 17:57:13 -0400

    Auto convert to class name

--- src/Vinelab/NeoEloquent/Eloquent/Model.php
+++ src/Vinelab/NeoEloquent/Eloquent/Model.php
@@ -706,4 +706,44 @@ abstract class Model extends IlluminateModel {
             return false;
         }
     }
+
+
+    /**
+     * Create a new instance of the given model.
+     *
+     * @param  array  $attributes
+     * @param  bool  $exists
+     * @return static
+     */
+    public function newInstance($attributes = [], $exists = false)
+    {
+        // This method just provides a convenient way for us to generate fresh model
+        // instances of this current model. It is particularly useful during the
+        // hydration of new objects via the Eloquent query builder instances.
+
+        $class = $attributes['className'] ?? static::class;
+        $model = new $class((array) $attributes);
+
+        $model->exists = $exists;
+
+        return $model;
+    }
+
+    /**
+     * Create a new model instance that is existing.
+     *
+     * @param  array  $attributes
+     * @param  string|null  $connection
+     * @return static
+     */
+    public function newFromBuilder($attributes = [], $connection = null)
+    {
+        $model = $this->newInstance($attributes, true);
+
+        $model->setRawAttributes((array) $attributes, true);
+
+        $model->setConnection($connection ?: $this->connection);
+
+        return $model;
+    }
 }
