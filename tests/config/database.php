53aaa6d6f65f9399878ff000188cec0e256a5c0a
Kinane <kinane.domloje@gmail.com>
2017-06-14 21:25:29 +0300

    remove test environment variable

--- tests/config/database.php
+++ tests/config/database.php
@@ -8,7 +8,7 @@ return array(
 
         'neo4j' => array(
             'driver' => 'neo4j',
-            'host' => 'dev',
+            'host' => 'localhost',
             'port' => 7474,
             'username' => 'neo4j',
             'password' => 'test',
@@ -16,8 +16,7 @@ return array(
 
         'default' => array(
             'driver' => 'neo4j',
-            'host' => 'dev',
-            'port' => 7474,
+            'host' => 'localhost',
             'username' => 'neo4j',
             'password' => 'test',
         ),
