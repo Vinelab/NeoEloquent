8790ca998ce29042c5dc83731ce04fae037c9234
marshy2346 <marshalex28@yahoo.com>
2017-07-10 12:41:16 -0400

    Added localhost and port information to default connection

--- tests/config/database.php
+++ tests/config/database.php
@@ -15,7 +15,11 @@ return array(
         ),
 
         'default' => array(
-            'driver' => 'neo4j'
+            'driver' => 'neo4j',
+            'host' => 'localhost',
+            'port' => 7474,
+            'user' => 'neo4j',
+            'password' => 'neo4j'
         )
     )
 );
