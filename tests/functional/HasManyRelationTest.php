9f4c81d554655303e9eda701b8a2512dcc17b7f6
ghlen <ghlen@pm.me>
2022-11-24 16:00:43 +0530

    fixed hasMany relation test

--- tests/functional/HasManyRelationTest.php
+++ tests/functional/HasManyRelationTest.php
@@ -133,15 +133,12 @@ class HasManyRelationTest extends TestCase
         ];
 
         foreach ($novel as $book) {
-            $edge = $author->books()->create($book, ['on' => $book['release_date']]);
+            $edge = $author->books()->create($book);
 
-            $this->assertInstanceOf('Vinelab\NeoEloquent\Eloquent\Edges\EdgeOut', $edge);
+            $this->assertInstanceOf(Book::class, $edge);
             $this->assertTrue($edge->exists());
-            $this->assertGreaterThan(0, $edge->id);
             $this->assertNotNull($edge->created_at);
             $this->assertNotNull($edge->updated_at);
-            $this->assertEquals($edge->on, $book['release_date']);
-            $edge->delete();
         }
     }
 
@@ -184,119 +181,5 @@ class HasManyRelationTest extends TestCase
         $booksIds = array_map(function ($book) { return $book->getKey(); }, $novel);
 
         $this->assertEquals(['A Game of Thrones', 'A Clash of Kings', 'A Storm of Swords', 'A Feast for Crows'], $booksIds);
-    }
-
-    public function testSavingManyRelationsWithRelationProperties()
-    {
-        $author = Author::create(['name' => 'George R. R. Martin']);
-
-        $novel = [
-            new Book([
-                'title' => 'A Game of Thrones',
-                'pages' => 704,
-                'release_date' => 'August 1996',
-            ]),
-            new Book([
-                'title' => 'A Clash of Kings',
-                'pages' => 768,
-                'release_date' => 'February 1999',
-            ]),
-            new Book([
-                'title' => 'A Storm of Swords',
-                'pages' => 992,
-                'release_date' => 'November 2000',
-            ]),
-            new Book([
-                'title' => 'A Feast for Crows',
-                'pages' => 753,
-                'release_date' => 'November 2005',
-            ]),
-        ];
-
-        $edges = $author->books()->saveMany($novel, ['novel' => true]);
-        $this->assertCount(count($novel), $edges->toArray());
-
-        foreach ($edges as $edge) {
-            $this->assertTrue($edge->novel);
-            $edge->delete();
-        }
-    }
-
-    public function testSyncingModelIds()
-    {
-        $author = Author::create(['name' => 'George R.R. Martin']);
-        $bk = Book::create(['title' => 'foo']);
-        $got = Book::create(['title' => 'A Game of Thrones', 'pages' => '704', 'release_date' => 'August 1996']);
-        $cok = Book::create(['title' => 'A Clash of Kings', 'pages' => '768', 'release_date' => 'February 1999']);
-
-        $author->books()->attach($bk);
-
-        $author->books()->sync([$got->id, $cok->id]);
-
-        $edges = $author->books()->edges();
-
-        $edgesIds = array_map(function ($edge) { return $edge->getRelated()->getKey(); }, $edges->toArray());
-
-        $this->assertTrue(in_array($got->id, $edgesIds));
-        $this->assertTrue(in_array($cok->id, $edgesIds));
-        $this->assertFalse(in_array($bk->id, $edgesIds));
-    }
-
-    public function testSyncingWithIdsUpdatesModels()
-    {
-        $author = Author::create(['name' => 'George R.R. Martin']);
-        $got = Book::create(['title' => 'A Game of Thrones', 'pages' => '704', 'release_date' => 'August 1996']);
-        $cok = Book::create(['title' => 'A Clash of Kings', 'pages' => '768', 'release_date' => 'February 1999']);
-        $sos = Book::create(['title' => 'A Storm of Swords', 'pages' => 992, 'release_date' => 'November 2000']);
-
-        $author->books()->attach($got);
-
-        $author->books()->sync([$got->id, $cok->id, $sos->id]);
-
-        $edges = $author->books()->edges();
-
-        $edgesIds = array_map(function ($edge) { return $edge->getRelated()->getKey(); }, $edges->toArray());
-
-        $this->assertTrue(in_array($got->id, $edgesIds));
-        $this->assertTrue(in_array($cok->id, $edgesIds));
-        $this->assertTrue(in_array($sos->id, $edgesIds));
-    }
-
-    public function testSyncingWithAttributes()
-    {
-        $author = Author::create(['name' => 'George R.R. Martin']);
-        $got = Book::create(['title' => 'A Game of Thrones', 'pages' => '704', 'release_date' => 'August 1996']);
-        $cok = Book::create(['title' => 'A Clash of Kings', 'pages' => '768', 'release_date' => 'February 1999']);
-        $sos = Book::create(['title' => 'A Storm of Swords', 'pages' => 992, 'release_date' => 'November 2000']);
-
-        $author->books()->attach($got);
-
-        $author->books()->sync([
-            $got->id => ['series' => 'Game'],
-            $cok->id => ['series' => 'Clash'],
-            $sos->id => ['series' => 'Storm'],
-        ]);
-
-        $edges = $author->books()->edges();
-
-        $edgesIds = array_map(function ($edge) { return $edge->getRelated()->getKey(); }, $edges->toArray());
-
-        $count = array_count_values((array) $got->id);
-
-        $this->assertEquals(1, $count[$got->id]);
-        $this->assertTrue(in_array($cok->id, $edgesIds));
-        $this->assertTrue(in_array($sos->id, $edgesIds));
-        $this->assertTrue(in_array($got->id, $edgesIds));
-
-        $expectedEdgesTypes = array('Storm', 'Clash', 'Game');
-
-        foreach ($edges as $key => $edge) {
-            $attributes = $edge->toArray();
-            $this->assertArrayHasKey('series', $attributes);
-            $this->assertTrue(in_array($edge->series, $expectedEdgesTypes));
-            $index = array_search($edge->series, $expectedEdgesTypes);
-            unset($expectedEdgesTypes[$index]);
-            $edge->delete();
-        }
     }
 }
