eb74aa2d93e12213635724efb2bd60fb51fe2c23
Ghlen Nagels <ghlen@pm.me>
2023-06-29 12:24:21 +0530

    fixed orders and limits test

--- tests/Functional/OrdersAndLimitsTest.php
+++ tests/Functional/OrdersAndLimitsTest.php
@@ -3,7 +3,7 @@
 namespace Vinelab\NeoEloquent\Tests\Functional;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Vinelab\NeoEloquent\Tests\Fixtures\Click;
+use Vinelab\NeoEloquent\Tests\Fixtures\Role;
 use Vinelab\NeoEloquent\Tests\TestCase;
 
 class OrdersAndLimitsTest extends TestCase
@@ -12,11 +12,11 @@ class OrdersAndLimitsTest extends TestCase
 
     public function testFetchingOrderedRecords()
     {
-        $c1 = Click::create(['num' => 1]);
-        $c2 = Click::create(['num' => 2]);
-        $c3 = Click::create(['num' => 3]);
+        $c1 = Role::create(['title' => 'a']);
+        $c2 = Role::create(['title' => 'b']);
+        $c3 = Role::create(['title' => 'c']);
 
-        $clicks = Click::orderBy('num', 'desc')->get();
+        $clicks = Role::orderBy('title', 'desc')->get();
 
         $this->assertCount(3, $clicks);
 
@@ -24,7 +24,7 @@ class OrdersAndLimitsTest extends TestCase
         $this->assertEquals($c2->toArray(), $clicks[1]->toArray());
         $this->assertEquals($c1->toArray(), $clicks[2]->toArray());
 
-        $asc = Click::orderBy('num', 'asc')->get();
+        $asc = Role::orderBy('title', 'asc')->get();
 
         $this->assertEquals($c1->toArray(), $asc[0]->toArray());
         $this->assertEquals($c2->toArray(), $asc[1]->toArray());
@@ -33,17 +33,17 @@ class OrdersAndLimitsTest extends TestCase
 
     public function testFetchingLimitedOrderedRecords()
     {
-        $c1 = Click::create(['num' => 1]);
-        $c2 = Click::create(['num' => 2]);
-        $c3 = Click::create(['num' => 3]);
+        $c1 = Role::create(['title' => 'a']);
+        $c2 = Role::create(['title' => 'b']);
+        $c3 = Role::create(['title' => 'c']);
 
-        $click = Click::orderBy('num', 'desc')->take(1)->get();
+        $click = Role::orderBy('title', 'desc')->take(1)->get();
         $this->assertCount(1, $click);
-        $this->assertEquals($c3->num, $click[0]->num);
+        $this->assertEquals($c3->title, $click[0]->title);
 
-        $another = Click::orderBy('num', 'asc')->take(2)->get();
+        $another = Role::orderBy('title', 'asc')->take(2)->get();
         $this->assertCount(2, $another);
-        $this->assertEquals($c1->num, $another[0]->num);
-        $this->assertEquals($c2->num, $another[1]->num);
+        $this->assertEquals($c1->title, $another[0]->title);
+        $this->assertEquals($c2->title, $another[1]->title);
     }
 }
