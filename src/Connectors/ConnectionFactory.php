c22f43864635ecaa64f4a8c045c2f6ba4bc011ef
Ghlen Nagels <ghlen@pm.me>
2023-09-20 11:01:20 -0400

    split connection logic into two seperate methods for improved useability

--- src/Connectors/ConnectionFactory.php
+++ src/Connectors/ConnectionFactory.php
@@ -6,7 +6,9 @@ use Illuminate\Database\Connectors\ConnectorInterface;
 use Laudis\Neo4j\Authentication\Authenticate;
 use Laudis\Neo4j\Basic\Driver;
 use Laudis\Neo4j\Common\Uri;
+use Laudis\Neo4j\Contracts\AuthenticateInterface;
 use Laudis\Neo4j\Databags\DriverConfiguration;
+use Psr\Http\Message\UriInterface;
 
 final class ConnectionFactory implements ConnectorInterface
 {
@@ -21,12 +23,23 @@ final class ConnectionFactory implements ConnectorInterface
      * @psalm-suppress MoreSpecificImplementedParamType
      * @psalm-suppress ImplementedReturnTypeMismatch
      *
-     * @param  array{scheme?: string, driver: string, host?: string, port?: string|int, username ?: string, password ?: string, database ?: string, prefix ?: string}  $config
+     * @param array{scheme?: string, driver: string, host?: string, port?: string|int, username ?: string, password ?: string, database ?: string, prefix ?: string} $config
      */
     public function connect(array $config): Driver
     {
+        [$uri, $config, $auth] = $this->toBaseConnectionParts($config);
+
+        return Driver::create($uri, $config, $auth);
+    }
+
+    /**
+     * @param array{scheme?: string, driver: string, host?: string, port?: string|int, username ?: string, password ?: string, database ?: string, prefix ?: string} $config
+     * @return array{0: UriInterface, 1: DriverConfiguration, 2: AuthenticateInterface}
+     */
+    public function toBaseConnectionParts(array $config): array
+    {
         $port = $config['port'] ?? null;
-        $port = (is_null($port) || $port === '') ? null : ((int) $port);
+        $port = (is_null($port) || $port === '') ? null : ((int)$port);
         $uri = $this->defaultUri->withScheme($config['scheme'] ?? '')
             ->withHost($config['host'] ?? '')
             ->withPort($port);
@@ -37,6 +50,6 @@ final class ConnectionFactory implements ConnectorInterface
             $auth = Authenticate::disabled();
         }
 
-        return Driver::create($uri, DriverConfiguration::default(), $auth);
+        return [ $uri, DriverConfiguration::default(), $auth ];
     }
 }
