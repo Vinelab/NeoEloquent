b4e059656a2ce8263e846d8bea0c32ffcdcd4cb5
Tachi <zaveruha007@gmail.com>
2023-10-30 21:01:35 +0200

    - fixed make:migration command

--- src/Console/Migrations/MigrateMakeCommand.php
+++ src/Console/Migrations/MigrateMakeCommand.php
@@ -2,6 +2,7 @@
 
 namespace Vinelab\NeoEloquent\Console\Migrations;
 
+use Exception;
 use Illuminate\Support\Composer;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
@@ -19,105 +20,72 @@ class MigrateMakeCommand extends BaseCommand
      */
     protected $description = 'Create a new migration file';
 
-    /**
-     * @var \Vinelab\NeoEloquent\Migrations\MigrationCreator
-     */
-    protected $creator;
-
-    /**
-     * The path to the packages directory (vendor).
-     *
-     * @var string
-     */
-    protected $packagePath;
-
-    /**
-     * @var \Illuminate\Foundation\Composer
-     */
-    protected $composer;
-
-    /**
-     * @param \Vinelab\NeoEloquent\Migrations\MigrationCreator $creator
-     * @param string                                           $packagePath
-     */
-    public function __construct(MigrationCreator $creator, Composer $composer, $packagePath)
+    public function __construct(protected MigrationCreator $creator, protected Composer $composer, protected string $packagePath)
     {
         parent::__construct();
 
-        $this->creator = $creator;
-        $this->packagePath = $packagePath;
-        $this->composer = $composer;
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    public function fire()
+    public function handle(): void
     {
         // It's possible for the developer to specify the tables to modify in this
         // schema operation. The developer may also specify if this label needs
         // to be freshly created so we can create the appropriate migrations.
-        $name = $this->input->getArgument('name');
+        $name = $this->input->getArgument(name: 'name');
 
-        $label = $this->input->getOption('label');
+        $label = $this->input->getOption(name: 'label');
 
-        $modify = $this->input->getOption('create');
+        $modify = $this->input->getOption(name: 'create');
 
-        if (!$label && is_string($modify)) {
+        if(!$label && \is_string(value: $modify)) {
             $label = $modify;
         }
 
         // Now we are ready to write the migration out to disk. Once we've written
         // the migration out, we will dump-autoload for the entire framework to
         // make sure that the migrations are registered by the class loaders.
-        $this->writeMigration($name, $label);
+        $this->writeMigration(name: $name, label: $label);
 
         $this->composer->dumpAutoloads();
     }
 
     /**
-     * Write the migration file to disk.
-     *
-     * @param string $name
-     * @param string $label
-     * @param bool   $create
-     *
-     * @return string
+     * @throws Exception
      */
-    protected function writeMigration($name, $label)
+    protected function writeMigration(string $name, string $label = null): void
     {
         $path = $this->getMigrationPath();
 
-        $file = pathinfo($this->creator->create($name, $path, $label), PATHINFO_FILENAME);
+        $file = pathinfo(path: $this->creator->create(name: $name, path: $path, table: $label), flags: PATHINFO_FILENAME);
 
-        $this->line("<info>Created Migration:</info> $file");
+        $this->line(string: "<info>Created Migration:</info> $file");
     }
 
     /**
      * {@inheritDoc}
      */
-    protected function getArguments()
+    protected function getArguments(): array
     {
-        return array(
-            array('name', InputArgument::REQUIRED, 'The name of the migration'),
-        );
+        return [
+            ['name', InputArgument::REQUIRED, 'The name of the migration'],
+        ];
     }
 
     /**
      * {@inheritDoc}
      */
-    protected function getOptions()
+    protected function getOptions(): array
     {
-        return array(
-            array('bench', null, InputOption::VALUE_OPTIONAL, 'The workbench the migration belongs to.', null),
+        return [
+            ['bench', null, InputOption::VALUE_OPTIONAL, 'The workbench the migration belongs to.', null],
 
-            array('create', null, InputOption::VALUE_OPTIONAL, 'The label schema to be created.'),
+            ['create', null, InputOption::VALUE_OPTIONAL, 'The label schema to be created.'],
 
-            array('package', null, InputOption::VALUE_OPTIONAL, 'The package the migration belongs to.', null),
+            ['package', null, InputOption::VALUE_OPTIONAL, 'The package the migration belongs to.', null],
 
-            array('path', null, InputOption::VALUE_OPTIONAL, 'Where to store the migration.', null),
+            ['path', null, InputOption::VALUE_OPTIONAL, 'Where to store the migration.', null],
 
-            array('label', null, InputOption::VALUE_OPTIONAL, 'The label to migrate.'),
-        );
+            ['label', null, InputOption::VALUE_OPTIONAL, 'The label to migrate.'],
+        ];
     }
 }
